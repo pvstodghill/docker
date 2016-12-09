7e9caff6ea2e0368667e04c4769f8fb9a1961db5
Paul Stodghill <paul.stodghill@ars.usda.gov>
2016-12-09 15:37:47 -0500

    breseq-0.28.1 build was completely broken. Fixed.
    - Was missing R and bowtie.
    - Use binary instead of building from source.

--- breseq/breseq-0.28.1/build.bash
+++ breseq/breseq-0.28.1/build.bash
@@ -5,11 +5,16 @@ VERSION=`egrep '^ENV +VERSION +' Dockerfile | sed -r -e 's/ENV +VERSION +//'`
 IMAGE=pvstodghill/$PACKAGE
 RELEASE=`date +%Y-%m-%d`
 
+BVERSION=`egrep '^ENV +BVERSION +' Dockerfile | sed -r -e 's/ENV +BVERSION +//'`
+
 fulltag=${IMAGE}:${VERSION}__${RELEASE}
 
 set -e
 set -x
 
+rm -rf bowtie2-${BVERSION}
+unzip bowtie2-${BVERSION}-linux-x86_64.zip
 docker build --pull -t ${fulltag} .
+rm -rf bowtie2-${BVERSION}
 docker run ${fulltag} breseq --version
 docker push ${fulltag}
