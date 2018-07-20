840a23f556cf0fd574ff869efd3f27995b921310
Paul Stodghill <paul.stodghill@ars.usda.gov>
2018-07-20 10:33:28 -0400

    Added Trimmomatic-0.38

---
+++ trimmomatic/Trimmomatic-0.38/build.bash
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+PACKAGE=`egrep '^ENV +PACKAGE +' Dockerfile | sed -r -e 's/ENV +PACKAGE +//'`
+VERSION=`egrep '^ENV +VERSION +' Dockerfile | sed -r -e 's/ENV +VERSION +//'`
+IMAGE=pvstodghill/$PACKAGE
+RELEASE=`date +%Y-%m-%d`
+
+fulltag=`echo ${IMAGE}:${VERSION}__${RELEASE} | tr '[A-Z]' '[a-z]'`
+
+set -e
+set -x
+
+docker build --pull -t ${fulltag} .
+docker run --rm ${fulltag} java -jar /opt/Trimmomatic/trimmomatic.jar -version
+docker push ${fulltag}
+
